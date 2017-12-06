21c1103065620caf197bee8f3367770bfbdcfe38
damienfauveau <damien.fauveau49@gmail.com>
2017-12-06 09:57:21 +0100

    ajout de la class magasin

--- LISA/Entities/Magasin.cs
+++ LISA/Entities/Magasin.cs
@@ -8,36 +8,10 @@ namespace LISA.Entities
 {
     class Magasin
     {
-        #region properties
-        /// <summary>
-        /// Code attribué au magasin
-        /// </summary>
-        public string Code { get; set; }
+        public int Id { get; set; }
 
-        /// <summary>
-        /// Nom de la chaine de magasin
-        /// </summary>
-        public string Marque { get; set; }
-
-        /// <summary>
-        /// Ville du magasin
-        /// </summary>
         public string Ville { get; set; }
 
-        /// <summary>
-        /// Code postal de la ville du magasin
-        /// </summary>
-        public int CodePostal { get; set; }
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Obtient le hashcode de l'objet
-        /// </summary>
-        public override int GetHashCode()
-        {
-            return !string.IsNullOrEmpty(Code) ? Code.GetHashCode() : 0;
-        }
-        #endregion
+        public string CodePostal { get; set; }
     }
 }
