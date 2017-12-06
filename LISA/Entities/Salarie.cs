643b299028376902b63488e9fcc8105646694bf1
damienfauveau <damien.fauveau49@gmail.com>
2017-12-06 14:22:57 +0100

    ajout class salarie

--- LISA/Entities/Salarie.cs
+++ LISA/Entities/Salarie.cs
@@ -8,5 +8,37 @@ namespace LISA.Entities
 {
     class Salarie
     {
+        #region properties
+
+        /// <summary>
+        /// Id associé au salarié
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nom du salarié
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Prénom du salarié
+        /// </summary>
+        public string Prenom { get; set; }
+
+        /// <summary>
+        /// Email du salarié
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Numéro de téléphone du salarié
+        /// </summary>
+        public int Telephone { get; set; }
+
+        /// <summary>
+        /// Mot de passe du salarié
+        /// </summary>
+        public string Password { get; set; }
+        #endregion
     }
 }
