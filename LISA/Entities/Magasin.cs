2382754479e7c949999fffc987acd07a988389c9
damienfauveau <damien.fauveau49@gmail.com>
2017-12-06 10:49:16 +0100

    ajout commentaires class magasin

--- LISA/Entities/Magasin.cs
+++ LISA/Entities/Magasin.cs
@@ -8,10 +8,21 @@ namespace LISA.Entities
 {
     class Magasin
     {
+        #region properties
+        /// <summary>
+        /// Identifiant du magasin
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Ville du magasin
+        /// </summary>
         public string Ville { get; set; }
 
+        /// <summary>
+        /// Code postal de la ville du magasin
+        /// </summary>
         public string CodePostal { get; set; }
+        #endregion
     }
 }
