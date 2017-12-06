6a9d905c000ada31fcd8bb3db1ca3a5f5ddf223d
damienfauveau <damien.fauveau49@gmail.com>
2017-12-06 09:38:44 +0100

    premier commit

---
+++ LISA/Entities/Magasin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISA.Entities
+{
+    class Magasin
+    {
+        #region properties
+        /// <summary>
+        /// Code attribué au magasin
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Nom de la chaine de magasin
+        /// </summary>
+        public string Marque { get; set; }
+
+        /// <summary>
+        /// Ville du magasin
+        /// </summary>
+        public string Ville { get; set; }
+
+        /// <summary>
+        /// Code postal de la ville du magasin
+        /// </summary>
+        public int CodePostal { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Obtient le hashcode de l'objet
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return !string.IsNullOrEmpty(Code) ? Code.GetHashCode() : 0;
+        }
+        #endregion
+    }
+}
