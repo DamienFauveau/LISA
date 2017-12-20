1f034d217809a67a8c609cc50e4fcb6e32346ca6
damienfauveau <damien.fauveau49@gmail.com>
2017-12-20 09:33:12 +0100

    back

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,19 +13,10 @@ namespace ApplicationConsole
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Dal<Client> clientDal = new Dal<Client>();
-            Client test = clientDal.FindById(1);
-            test.Email = "dsqdsqdsqd@hotmail.fr";
-            clientDal.Update();
-=======
             Dal<Catalogue> catalogueDal = new Dal<Catalogue>();
             Catalogue test = catalogueDal.FindById(1);
             Console.WriteLine(test.Width);
             Console.ReadKey(); 
-            //test.Label = "Test update";
-            //catalogueDal.Update(test);
->>>>>>> f88023c61996fd4b51041bb0db9d54c7eeb94077
         }
     }
 }
