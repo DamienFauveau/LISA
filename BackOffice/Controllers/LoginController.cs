b3c30a0ba43005466c7f48d4d624eea6e16de643
Dylan <alizon.dylan@hotmail.fr>
2017-12-21 13:58:04 +0100

    gestion token dans back office 2

---
+++ BackOffice/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using BackOffice.Models;
+using BackOffice.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackOffice.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Route("Connexion")]
+        public ActionResult Connexion()
+        {
+            return View();
+        }
+
+        [Route("Connexion")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Connexion([Bind(Include = "Login,Password")] LoginVM connexion)
+        {
+            string responseString = "";
+            if (ModelState.IsValid)
+            {
+                responseString = HttpClientService.Connexion(connexion);
+                if(responseString.Length > 0)
+                {
+                    TokenVM token = JsonConvert.DeserializeObject<TokenVM>(responseString);
+                    Session["token"] = token.Token;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(connexion);
+        }
+    }
+}
---
+++ BackOffice/Service/HttpClientService.cs
@@ -0,0 +1,31 @@
+﻿using BackOffice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BackOffice.Service
+{
+    public class HttpClientService
+    {
+        public static string Connexion(LoginVM connexion)
+        {
+            string responseBody = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:53334/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("Salaries/Connexion", connexion).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    responseBody = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            return responseBody;
+        }
+    }
+}
